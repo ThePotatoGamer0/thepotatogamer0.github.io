199434a67e23b8af5acebff13425f1d448630c3a
ThePotatoGamer0 <102364508+ThePotatoGamer0@users.noreply.github.com>
2025-03-18 01:23:35 +0000

    Add page leave animations

--- Javascript/Loader.js
+++ Javascript/Loader.js
@@ -1,19 +1,22 @@
-// Get the div element with the class "content"
+// Get the div elements with the class "content" and "loader"
 const contentDiv = document.querySelector('.content');
 const loaderDiv = document.querySelector('.loader');
-// Initially hide the div
+// Initially hide the content div and show the loader div
 contentDiv.style.display = 'none';
 contentDiv.style.opacity = '0';
 loaderDiv.style.display = 'block';
+//Create the function that hides the loader
 function loaderDisappear() {
     loaderDiv.style.display = 'none';
 }
+//Create the function that hides the loader and shows the content
 function LoaderEnd() {
     loaderDiv.classList.add('fade-out');
     contentDiv.style.display = 'block';
     contentDiv.classList.add('fade-in');
     setTimeout(loaderDisappear, 500)
 }
+// Perform Switch
 if (contentDiv.complete) {
     setTimeout(LoaderEnd, 500); // Call the function immediately if the div is already loaded
   } else {
